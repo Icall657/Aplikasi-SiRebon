d362ed1d0acf9fecee48c7c55d9f6b4f7852351a
ikbalaja <ikbalshfa@gmail.com>
2024-10-24 10:10:56 +0700

    menambahkan fitur admin dan fitur user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -43,18 +43,25 @@
             <hr class="sidebar-divider my-0">
 
             <!-- Nav Item - Dashboard -->
+            @if (auth()->user()->level == "admin")
             <li class="nav-item active">
                 <a class="nav-link" href="#">
                     <i class="fa fa-home"></i>
-                    <span>Beranda</span></a>
+                    <span>beranda</span></a>
             </li>
+            @endif
+
             <!-- Divider -->
+            @if (auth()->user()->level == "user")
             <hr class="sidebar-divider">
             <li class="nav-item active">
                 <a class="nav-link" href="#">
                     <i class="fa fa-user"></i>
                     <span>Profil</span></a>
             </li>
+            @endif
+            
+            
 
             <!-- Divider -->
             <hr class="sidebar-divider">
@@ -327,7 +334,7 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ auth()->user()->name }}</span>
                                 <img class="img-profile rounded-circle"
                                     src="img/undraw_profile.svg">
                             </a>
