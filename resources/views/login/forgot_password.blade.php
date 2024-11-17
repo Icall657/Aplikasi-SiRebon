116c1a3658ebc8313ea6e8e14463029d52c9ee18
MyBook14E <faisalhafidz0605@gmail.com>
2024-11-17 21:05:38 +0700

    membenarkan front end lupa password

--- resources/views/login/forgot_password.blade.php
+++ resources/views/login/forgot_password.blade.php
@@ -8,43 +8,57 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="{{ asset('StyleLogin/Forgot-Password/style.css') }}" rel="stylesheet">
-    <title>Lupa Password</title>
+    <title>Lupa Password SiRebon</title>
 </head>
-
+<style>
+    body {
+        background-color: #4E73DF;
+    }
+</style>
 <body>
-    <div class="container-fluid ">
-        <form class="mx-auto" action="{{ route('password.email') }}" method="post">
-            @csrf
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card profile-card">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Lupa Password</h5>
+                        <hr>
+                        <form action="{{ route('password.email') }}" method="post">
+                            @csrf
 
-            <h4 class="text-center mb-5">Lupa <span>Password</span></h4>
-            @if (session('status'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('status') }}
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+
+                            @if ($errors->any())
+                                <div class="alert alert-danger">
+                                    @foreach ($errors->all() as $error)
+                                        <p class="mb-0">{{ $error }}</p>
+                                    @endforeach
+                                </div>
+                            @endif
+
+                            <div class="mb-3 mt-4">
+                                <label for="email" class="form-label">Masukkan Email</label>
+                                <input type="email" class="form-control @error('email') is-invalid @enderror" id="email"
+                                    name="email" required autofocus>
+                            </div>
+
+                            <button type="submit" class="btn btn-primary w-100">Kirim Link Reset Password</button>
+                            <a href="{{ route('login') }}" class="text-center d-block text-decoration-none mt-3">
+                                Kembali
+                            </a>
+                        </form>
+                    </div>
                 </div>
-            @endif
-
-            @if ($errors->any())
-                <div class="alert alert-danger mb-0">
-                    @foreach ($errors->all() as $error)
-                        <p>{{ $error }}</p>
-                    @endforeach
-                </div>
-            @endif
-
-            <div class="mb-3 mt-5">
-                <label for="email" class="form-label">Masukkan Email</label>
-                <input type="email" class="form-control @error('email') is-invalid @enderror" id="email"
-                    name="email" required autofocus>
-                <a href="{{ route('login') }}" class="link-primary text-dark fw-large d-block mt-3"
-                    style="text-decoration: none">Kembali</a>
             </div>
-            <button type="submit" class="btn btn-primary w-100">Kirim Link Reset Password</button>
-        </form>
+        </div>
     </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
-
 </body>
-
 </html>
